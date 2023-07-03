4bca2d04460e26eeb2dee1f557daa8c36f11c933
gitdela <scriptingdela@gmail.com>
2023-07-03 09:16:34 +0000

    Fix responsiveness in index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ['./docs/**/*.{html,js}'],
   theme: {
+    screens: {
+      xxs: '315px',
+      xs: '350px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
     extend: {},
   },
   plugins: [],
