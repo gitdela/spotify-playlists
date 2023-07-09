f2f7d589ef4cc3bf45eab35fe9bb5bf845413f04
gitdela <scriptingdela@gmail.com>
2023-07-09 17:55:29 +0000

    Add refresh token function

--- docs/index.js
+++ docs/index.js
@@ -2,9 +2,13 @@ const spLogin = document.querySelector('#sp-login');
 
 function constrLink() {
   let clientId = 'dbfef9f44c2d4cd5a0ea254e1b42a559';
-  let redirectUri = 'https://gitdela.github.io/spotify-playlists/main.html';
+  let redirectUri = 'http://127.0.0.1:5500/docs/main.html';
   let scopes =
     'playlist-modify-private playlist-modify-public user-top-read user-library-modify user-library-read';
+  let array = new Uint32Array(1);
+  window.crypto.getRandomValues(array);
+  let state = array[0].toString();
+
   let authorizationUrl =
     'https://accounts.spotify.com/authorize?' +
     'client_id=' +
@@ -12,12 +16,16 @@ function constrLink() {
     '&response_type=code' +
     '&redirect_uri=' +
     encodeURIComponent(redirectUri) +
+    '&state=' +
+    state +
     '&scope=' +
-    encodeURIComponent(scopes);
+    encodeURIComponent(scopes) +
+    '&show_dialog=true';
 
   console.log(authorizationUrl);
 
   window.location.href = authorizationUrl;
 }
-
 spLogin.addEventListener('click', constrLink);
+
+// http://127.0.0.1:5500/docs/main.html
--- docs/main.js
+++ docs/main.js
@@ -1,75 +1,71 @@
-// so you set up the log in page and the user granted permission by login in to spotify
-// what that means is spotify will give you an authorization code
-// this is the first code of the OAuth 2.0
-// this authorization code just lets us get access to the access token, that is it
-// it expires quickly. but the app will use it fast enough
-// the access token is what we will actually use to access the user's data and modify it
-// it comes in the url and we need to extract it
-// so let's write the function for that
-
 function extractAuthorizationCode() {
   const urlParams = new URLSearchParams(window.location.search);
   const authorizationCode = urlParams.get('code');
   return authorizationCode;
 }
 
-// after getting the authorization code, we need to exchange it for the access token
-// according to spotify, we need the arguments you see in the function here
-// we just got the auth code, the rest you already have
-// it is supposed to go and fetch data from the network so it is an async function
 async function exchangeCodeForToken(
   authorizationCode,
   redirectUri,
   clientId,
   clientSecret
 ) {
-  // first let's store the token end point url inside a variable
   const tokenEndPointURL = 'https://accounts.spotify.com/api/token';
 
-  // according to the Spotify API documentation
-  // these details must be in the body of the qualifying object
-  // i need to read more about this
-  const data = {
-    grant_type: 'authorization_code',
-    code: authorizationCode,
-    redirect_uri: redirectUri,
-    client_id: clientId,
-    client_secret: clientSecret,
+  const headers = {
+    Authorization: 'Basic ' + btoa(clientId + ':' + clientSecret),
+    'Content-Type': 'application/x-www-form-urlencoded',
   };
 
-  // when we fetch, we need to use the url end point
-  // the second parameter is the data object we built
-  // this object contains the method, headers, body
-  // these are requirements from Spotify to know exactly what we are fetching and in what way
-  // also our authentication code is in there
-  // notice that because it is a fetch, a promise object will be returned hence we await on it
-  // this is where the async nature of this function is going to start
+  const data = new URLSearchParams();
+  data.append('grant_type', 'authorization_code');
+  data.append('code', authorizationCode);
+  data.append('redirect_uri', redirectUri);
 
   const response = await fetch(tokenEndPointURL, {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: new URLSearchParams(data),
+    headers: headers,
+    body: data,
   });
 
-  // the promise returned by the fetch() has finally been received and put inside of response
-  // but inside response, the data we need isn't in js object format yet so we need to parse it
-  // we use .json() method and it also returns a promise so we wait on it
-  // finally we put the resultant js object inside tokenData
   const tokenData = await response.json();
-  // with the tokenData js object that contains the access_token key, we can extract the needed access token
+  console.log(tokenData);
   const accessToken = tokenData.access_token;
-  // this is not safe, storing the access token in local storage but i don't have any other way for now
   localStorage.setItem('access_token_key', accessToken);
+  const refreshToken = tokenData.refresh_token;
+  localStorage.setItem('refresh_token_key', refreshToken);
+  const accessTime = Math.floor(Date.now() / 1000);
+  localStorage.setItem('access_time', JSON.stringify(accessTime));
   console.log('new access: ' + accessToken);
   return accessToken;
 }
 
-// after getting the access token, from the  spotify endpoint,
-// we can use it now to access the user data based on the scopes the app specified
-// in this app, we are going to add a specific playlist ID and search Spotify with it
-// again, we are fetching so this is an async function. declare it as such
+async function refreshAccessToken(refreshToken, clientId, clientSecret) {
+  const tokenEndPointURL = 'https://accounts.spotify.com/api/token';
+  const headers = {
+    Authorization: 'Basic ' + btoa(clientId + ':' + clientSecret),
+    'Content-Type': 'application/x-www-form-urlencoded',
+  };
+  const data = new URLSearchParams();
+  data.append('grant_type', 'refresh_token');
+  data.append('refresh_token', refreshToken);
+
+  const response = await fetch(tokenEndPointURL, {
+    method: 'POST',
+    headers: headers,
+    body: data,
+  });
+
+  const tokenData = await response.json();
+  const accessToken = tokenData.access_token;
+  console.log('refresh access token:', accessToken);
+
+  // Update the access token in local storage
+  localStorage.setItem('access_token_key', accessToken);
+
+  return accessToken;
+}
+
 async function fetchPlaylistById(playlistId, accessToken) {
   try {
     const response = await fetch(
@@ -87,29 +83,31 @@ async function fetchPlaylistById(playlistId, accessToken) {
 
     const data = await response.json();
     const playlistTracks = data.items;
+    console.log(playlistTracks);
     return playlistTracks;
   } catch (error) {
     console.error(error);
   }
 }
 
-async function fetchArtistGenre(trackArtistId, accessToken) {
-  const response = await fetch(
-    `https://api.spotify.com/v1/artists/${trackArtistId}`,
-    {
+async function fetchArtistGenres(trackArtistIds, accessToken) {
+  const genrePromises = trackArtistIds.map((artistId) =>
+    fetch(`https://api.spotify.com/v1/artists/${artistId}`, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
-    }
+    }).then((response) => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch track details');
+      }
+      return response.json();
+    })
   );
 
-  if (!response.ok) {
-    throw new Error('Failed to fetch track details');
-  }
-
-  const data = await response.json();
-  const artistGenre = data.genres;
-  return artistGenre;
+  const artistResponses = await Promise.all(genrePromises);
+  const artistGenres = artistResponses.map((response) => response.genres);
+  console.log(artistGenres);
+  return artistGenres;
 }
 
 async function getUserId(accessToken) {
@@ -138,6 +136,8 @@ function createPlaylistOnUI(matchedSongs) {
   console.log(matchedSongs);
   const playlistContainer = document.querySelector('#pl-con');
   const createPlaylistBtn = document.querySelector('#cr-pl-btn');
+  const fragment = document.createDocumentFragment();
+
   matchedSongs.forEach((track) => {
     const trackName = track.track.name;
     const trackArtist = track.track.artists[0].name;
@@ -150,16 +150,19 @@ function createPlaylistOnUI(matchedSongs) {
       'pl-2',
       'my-2'
     );
+
     const songSpan = document.createElement('span');
     songSpan.classList.add('text-md');
     const artistSpan = document.createElement('span');
     artistSpan.classList.add('text-xs');
-    songSpan.innerHTML = `${trackName}`;
-    artistSpan.innerHTML = `${trackArtist}`;
+    songSpan.textContent = `${trackName}`;
+    artistSpan.textContent = `${trackArtist}`;
     songsContainer.appendChild(songSpan);
     songsContainer.appendChild(artistSpan);
-    playlistContainer.appendChild(songsContainer);
+    fragment.appendChild(songsContainer);
   });
+
+  playlistContainer.appendChild(fragment);
   createPlaylistBtn.classList.remove('hidden');
 }
 
@@ -190,15 +193,10 @@ async function createPlaylist(
       throw new Error('Failed to create playlist');
     }
 
-    // after creating the playlist name, we need to add songs
-    // grab that playlist and get its id
-
     const data = await response.json();
     console.log(data);
     const playlistId = data.id;
     console.log(playlistId);
-
-    // now let's add songs to the playlist via it's id
 
     await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
       method: 'POST',
@@ -216,50 +214,40 @@ async function createPlaylist(
   }
 }
 
-// now we have the array of matchedSongs, let's create the playlist
-
-// this is the main controller function
-// it is the only one called globally after the app loads
-// when it is called, it checks if there is an existing access token in the localStorage
-// if there is, it puts it in a variable and because the if statement did not pass, it bypass it
-// directly to get the playlist id and go call the playlistData() to get the data and display
-// if it checks and there is no access token in localStorage, the if statement passes
-// it needs to get the authorization code then the access token
-// before going ahead to go get the playlistData()
-// we might be waiting on the accessToken before going to get the playlistData so this is an async function
 async function main() {
-  // when the page loads, we want to get an old access token we saved in local storage
   let accessToken = localStorage.getItem('access_token_key');
+  const redirectUri = 'http://127.0.0.1:5500/docs/main.html';
+  const clientId = 'dbfef9f44c2d4cd5a0ea254e1b42a559';
+  const clientSecret = '855f16e387cf4ae3b7907e6dc9375993'; //gitdela.github.io/spotify-playlists/main.html
 
-  // if that access token is not there, we need to get it
   if (!accessToken) {
-    // if there is no old access token, get the auth code to help us get it
     const authorizationCode = extractAuthorizationCode();
-    // now we have the authorization code, we need to send it to spotify and receive the access token
-    // according to them, we won't redirect the url again but they still need the redirect uri
-    // to exchange the auth code for the access token, we will fetch from the token endpoint
-    // that means it is going to be an async function hence we await it to resolve
-    const redirectUri = 'https://gitdela.github.io/spotify-playlists/main.html';
-    const clientId = 'dbfef9f44c2d4cd5a0ea254e1b42a559';
-    const clientSecret = '855f16e387cf4ae3b7907e6dc9375993';
-    // when the exchangeCodeForToken is executed, it will return the value to the accessToken variable here
+
     accessToken = await exchangeCodeForToken(
       authorizationCode,
       redirectUri,
       clientId,
       clientSecret
     );
+  } else {
+    const currentTime = Math.floor(Date.now() / 1000);
+    const savedTime = localStorage.getItem('access_time');
+    const timeDifference = currentTime - parseInt(savedTime);
+    const refreshToken = localStorage.getItem('refresh_token_key');
+
+    if (timeDifference > 3600) {
+      console.log('Access token has expired');
+      accessToken = await refreshAccessToken(
+        refreshToken,
+        clientId,
+        clientSecret
+      );
+    }
   }
 
   const playlistId = '37i9dQZEVXbLRQDuF5jeBp';
-  // notice that when the user comes in initially, accessToken declared for the localStorage was not used
-  // but it was still declared
-  // so even when the exchangeCode() returned the value to the prev if block, it still updated the localStorage accessToken
-  // it is not a separate/new variable
-  // that is how come we were able to pass its value to the fetchPlaylist() here even though it's not in the if block
-  // we will be fetching here too so do not forget to await it
   const playlistData = await fetchPlaylistById(playlistId, accessToken);
-  // console.log(playlistData);
+  console.log(playlistData);
   const wantedGenres = [
     'hiphop',
     'hip hop',
@@ -273,39 +261,29 @@ async function main() {
     'conscious hip hop',
   ];
   let matchedSongs = [];
-  const playlistContainer = document.querySelector('#pl-con');
-  // i was using forEach to iterate over the playlistData
-  // but to get the genre of the artist, i needed to fetch the track details again
-  // weird that Spotify does not include the track details in their playlists
-  // fetch is asynchronous but forEach does not support asynchronous behavior
-  // when you use the await keyword on forEach, you will get an error
-  // it does not wait for the promise to resolve
-  // for of loop does and that is why it is in use here. REMEMBER THAT
-  for (const track of playlistData) {
-    // const trackId = track.track.id;
-    const trackArtistID = track.track.artists[0].id;
-    const artistGenre = await fetchArtistGenre(trackArtistID, accessToken);
-    // console.log(artistGenre);
 
-    const matchedGenres = artistGenre.filter((genre) =>
+  const trackArtistIds = playlistData.map((track) => track.track.artists[0].id);
+  console.log(trackArtistIds);
+  const artistGenres = await fetchArtistGenres(trackArtistIds, accessToken);
+
+  for (i = 0; i < playlistData.length; i++) {
+    const track = playlistData[i];
+    const artistGenreArray = artistGenres[i];
+
+    const hasMatchingGenre = artistGenreArray.some((genre) =>
       wantedGenres.includes(genre)
     );
 
-    if (matchedGenres.length > 0) {
+    if (hasMatchingGenre) {
       matchedSongs.push(track);
-      // const trackElement = document.createElement('p');
-      // trackElement.textContent = `${trackName} - ${trackArtist}`;
-      // playlistContainer.appendChild(trackElement);
     }
   }
+  console.log(matchedSongs);
+
   const loadPlaylistBtn = document.querySelector('#load-pl-btn');
   loadPlaylistBtn.addEventListener('click', () => {
     createPlaylistOnUI(matchedSongs);
   });
-
-  // now we have the songs that we need
-  // we need to grab the current user's id and do things to him lol
-  // we have to fetch again. this time to the /me end point
 
   const currentUserId = await getUserId(accessToken);
   // console.log(currentUserId);
@@ -324,3 +302,5 @@ async function main() {
 }
 
 main();
+
+// https://gitdela.github.io/spotify-playlists/main.html
